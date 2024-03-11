fe8c926f6e3de5f61d9b01da36aa142cf4357a4c
Antony Braiano <78004742+Antony-Braiano@users.noreply.github.com>
2024-03-11 14:51:04 -0300

    Update and rename crDroid.mk to rising.mk

--- rising.mk
+++ rising.mk
@@ -9,9 +9,6 @@ TARGET_NO_KERNEL_OVERRIDE := true
 TARGET_NO_KERNEL_IMAGE := true
 SELINUX_IGNORE_NEVERALLOWS := true
 
-# OTA
-# PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-#    ro.system.ota.json_url=https://raw.githubusercontent.com/naz664/crDroid_gsi/14/ota.json
 
 # Set Bootanimation at 720P
 TARGET_BOOT_ANIMATION_RES := 720
